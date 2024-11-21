5842075b14446c3b7635e7553268166c311bf5ef
Bruno-Carara <bruno.tcarara@gmail.com>
2024-11-21 15:09:43 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,16 +12,24 @@ public class Main {
 		Map<Integer, Integer> Votos_Candidato= Leitor.ler_votos(arquivo);		
 		Map<Integer, Integer> Votos_Urna= Leitor.ler_urna(arquivo);	
 		int votos_válidos = Leitor.ler_válidos(arquivo);
-		
-		
-		
-		System.out.println(Leitor.ler_urna(arquivo));
-		
-		System.out.println(Leitor.ler_votos(arquivo));
-		
+		int votos_brancos = Leitor.ler_brancos(arquivo);
+		int votos_nulos = Leitor.ler_nulos(arquivo);
+		float[][] votos_partido = Relatório.Votos_Partido(Votos_Candidato);
+		int[][] votos_por_candidato = Relatório.Votos_Candidato(Votos_Candidato);
+		int[][] votos_por_urna = Relatório.Votos_Urna(Votos_Urna);
 		float QE = Relatório.Calcular_QE(votos_válidos);
+		float[][] QP = Relatório.Calcular_QP(votos_partido, QE);
 		
-		Relatório.Calcular_QP(Votos_Candidato);
+		
+		//Resultados
+		
+		System.out.println("Cálculo do Quociente Eleitoral (QE):" + QE + "\n");
+		Relatório.Mostrar_QP(QP);
+		Relatório.Mostrar_Votos_Urna(votos_por_urna);
+		Relatório.Total_Votos(votos_por_urna);
+		Relatório.Mostrar_Votos_Candidato(votos_por_candidato);
+		System.out.println("Total de Votos Brancos: " + votos_brancos + "\n");
+		System.out.println("Total de Votos Nulos: " + votos_nulos + "\n");
 	}
 
 }
