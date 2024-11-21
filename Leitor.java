37f7fd90b44a850e655fc100cc712bd2eeaed2c0
Bruno-Carara <bruno.tcarara@gmail.com>
2024-11-21 11:42:36 -0300

    Update Leitor.java

--- Leitor.java
+++ Leitor.java
@@ -9,7 +9,7 @@ public class Leitor extends Main{
 	private static int Nulos = 0;
 	private static int Brancos = 0;
 	private static int Válidos = 0;
-	
+
 	public int getNulos() {
 		return Nulos;
 	}
@@ -36,31 +36,31 @@ public class Leitor extends Main{
 
 	@SuppressWarnings("rawtypes")
 	public static HashMap ler_votos(String arquivo) {
-	
-				Map<Integer, Integer> Candidatos = new HashMap<>();
-		
-				try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
-					String linha;
-					while ((linha = br.readLine()) != null) {
-						String[] partes = linha.split(";");
-						int candidato = Integer.parseInt(partes[1]);
-						Candidatos.put(candidato, Candidatos.getOrDefault(candidato, 0) + 1);
-						if (candidato == 0) {
-							Nulos++;
-						} else if (candidato == 1) {
-							Brancos++;
-						} else {
-							Válidos++;
-						} 
-					}
-				} catch (IOException e) {
-					System.out.println("Erro ao ler o arquivo: " + e.getMessage());
-				}
-				return (HashMap) Candidatos;
+
+		Map<Integer, Integer> Candidatos = new HashMap<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+			String linha;
+			while ((linha = br.readLine()) != null) {
+				String[] partes = linha.split(";");
+				int candidato = Integer.parseInt(partes[1]);
+				Candidatos.put(candidato, Candidatos.getOrDefault(candidato, 0) + 1);
+				if (candidato == 0) {
+					Nulos++;
+				} else if (candidato == 1) {
+					Brancos++;
+				} else {
+					Válidos++;
+				} 
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao ler o arquivo: " + e.getMessage());
 		}
+		return (HashMap) Candidatos;
+	}
 	@SuppressWarnings("rawtypes")
 	public static HashMap ler_urna(String arquivo) {
-		
+
 		Map<Integer, Integer> Candidatos = new HashMap<>();
 
 		try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
@@ -74,5 +74,24 @@ public class Leitor extends Main{
 			System.out.println("Erro ao ler o arquivo: " + e.getMessage());
 		}
 		return (HashMap) Candidatos;
-}
+	}
+	public static int ler_válidos(String arquivo) {
+
+		Map<Integer, Integer> Candidatos = new HashMap<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+			String linha;
+			while ((linha = br.readLine()) != null) {
+				String[] partes = linha.split(";");
+				int candidato = Integer.parseInt(partes[1]);
+				Candidatos.put(candidato, Candidatos.getOrDefault(candidato, 0) + 1);
+				if ((candidato != 0) && (candidato != 1)) {
+					Válidos++;
+				} 
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+		}
+		return Válidos;
+	}
 }
