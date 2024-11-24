f8947a0059ead087a84101b54091e9007c21d4c2
Bruno-Carara <bruno.tcarara@gmail.com>
2024-11-24 17:19:18 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,17 +19,26 @@ public class Main {
 		int[][] votos_por_urna = Relatório.Votos_Urna(Votos_Urna);
 		float QE = Relatório.Calcular_QE(votos_válidos);
 		float[][] QP = Relatório.Calcular_QP(votos_partido, QE);
-		
+		int votos_total = Relatório.Total_Votos(votos_por_urna);
 		
 		//Resultados
 		
 		System.out.println("Cálculo do Quociente Eleitoral (QE):" + QE + "\n");
 		Relatório.Mostrar_QP(QP);
 		Relatório.Mostrar_Votos_Urna(votos_por_urna);
-		Relatório.Total_Votos(votos_por_urna);
+		System.out.println("Total de Votos: " + votos_total + "\n");
 		Relatório.Mostrar_Votos_Candidato(votos_por_candidato);
 		System.out.println("Total de Votos Brancos: " + votos_brancos + "\n");
 		System.out.println("Total de Votos Nulos: " + votos_nulos + "\n");
+		
+		System.out.println("Percentual de Votos Brancos: " + (float)votos_brancos/votos_total + "%\n");
+		System.out.println("Percentual de Votos Nulos: " + (float)votos_nulos/votos_total + "%\n");
+		System.out.println("Percentual de Votos Válidos: " + (float)votos_válidos/votos_total + "%\n");
+		
+		System.out.println("Candidatos Mais Votados:");
+		Relatório.Maiores_Candidatos(votos_por_candidato);
+		
+		Relatório.Eleitos();
 	}
 
 }
