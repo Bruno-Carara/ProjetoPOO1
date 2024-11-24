55bc3d81b17516bc7fecb0c61f2b7f6496312064
Bruno-Carara <bruno.tcarara@gmail.com>
2024-11-24 17:20:16 -0300

    Update Relatório.java

--- Relatório.java
+++ Relatório.java
@@ -1,7 +1,6 @@
 package Eleição;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Relatório extends Main{
@@ -17,18 +16,108 @@ public class Relatório extends Main{
 	private float porcentagem_brancos;
 	private float porcentagem_nulos;
 	private int partido;
+	
+	
+
+	public int getVotos_urna() {
+		return votos_urna;
+	}
+
+	public void setVotos_urna(int votos_urna) {
+		this.votos_urna = votos_urna;
+	}
+
+	public int getVotos_válidos() {
+		return votos_válidos;
+	}
+
+	public void setVotos_válidos(int votos_válidos) {
+		this.votos_válidos = votos_válidos;
+	}
+
+	public int getVotos_candidato() {
+		return votos_candidato;
+	}
+
+	public void setVotos_candidato(int votos_candidato) {
+		this.votos_candidato = votos_candidato;
+	}
+
+	public int getVotos_partido() {
+		return votos_partido;
+	}
+
+	public void setVotos_partido(int votos_partido) {
+		this.votos_partido = votos_partido;
+	}
+
+	public int getVotos_nulos() {
+		return votos_nulos;
+	}
+
+	public void setVotos_nulos(int votos_nulos) {
+		this.votos_nulos = votos_nulos;
+	}
+
+	public int getVotos_brancos() {
+		return votos_brancos;
+	}
+
+	public void setVotos_brancos(int votos_brancos) {
+		this.votos_brancos = votos_brancos;
+	}
+
+	public int getTotal_votos() {
+		return total_votos;
+	}
+
+	public void setTotal_votos(int total_votos) {
+		this.total_votos = total_votos;
+	}
+
+	public float getPorcentagem_válidos() {
+		return porcentagem_válidos;
+	}
+
+	public void setPorcentagem_válidos(float porcentagem_válidos) {
+		this.porcentagem_válidos = porcentagem_válidos;
+	}
+
+	public float getPorcentagem_brancos() {
+		return porcentagem_brancos;
+	}
+
+	public void setPorcentagem_brancos(float porcentagem_brancos) {
+		this.porcentagem_brancos = porcentagem_brancos;
+	}
+
+	public float getPorcentagem_nulos() {
+		return porcentagem_nulos;
+	}
+
+	public void setPorcentagem_nulos(float porcentagem_nulos) {
+		this.porcentagem_nulos = porcentagem_nulos;
+	}
+
+	public int getPartido() {
+		return partido;
+	}
+
+	public void setPartido(int partido) {
+		this.partido = partido;
+	}
 
 	public static float Calcular_QE(int votos_válidos) {
 		float QE = (votos_válidos/10);
 		return QE;
 	}
-	
-	public static void Total_Votos(int[][] Votos_Urna) {
+
+	public static int Total_Votos(int[][] Votos_Urna) {
 		int soma = 0;
 		for (int i = 0; i < Votos_Urna.length; i++) {
 			soma += Votos_Urna[i][1];
 		}
-		System.out.println("A soma de todos os votos é: " + soma + "\n");
+		return soma;
 	}
 
 	public static int[][] Votos_Candidato(Map<Integer, Integer> Votos_Candidato){
@@ -58,7 +147,7 @@ public class Relatório extends Main{
 		}
 		return Matriz_Candidatos;
 	}
-	
+
 	public static void Mostrar_Votos_Candidato(int[][] Matriz_Candidatos) {
 		System.out.println("Votos por Candidato:");
 		for (int i = 0; i < Matriz_Candidatos.length; i++) {
@@ -66,9 +155,9 @@ public class Relatório extends Main{
 		}
 		System.out.println("\n");
 	}
-	
 
-	
+
+
 
 	public static int[][] Votos_Urna(Map<Integer, Integer> Votos_Urna){
 		StringBuilder Votos_Map = new StringBuilder();
@@ -185,6 +274,40 @@ public class Relatório extends Main{
 			System.out.println("Partido " + Matriz_QP[i][0] + " = " + Matriz_QP[i][1]);
 		}
 		System.out.println("\n");
+	}
+
+	public static void Maiores_Candidatos(int[][] Matriz_Candidatos) {
+		int[][] Cadeiras = new int[25][2];
+		for (int i = 0; i < Matriz_Candidatos.length - 1; i++) {
+			for (int j = 0; j < Matriz_Candidatos.length - 1 - i; j++) {
+				if (Matriz_Candidatos[j][1] < Matriz_Candidatos[j + 1][1]) {
+
+					int[] x = Matriz_Candidatos[j];
+					Matriz_Candidatos[j] = Matriz_Candidatos[j + 1];
+					Matriz_Candidatos[j + 1] = x;
+				}
+			}
+		}
+		for (int i = 0; i < Cadeiras.length; i++) {
+			Cadeiras[i][0] = Matriz_Candidatos[i][0]; 
+			Cadeiras[i][1] = Matriz_Candidatos[i][1]; 
+		}
+		for (int i=0;i<Matriz_Candidatos.length;i++) {
+			Cadeiras[i][1] = i+1;
+		}
+		for (int i = 0; i < Cadeiras.length; i++) {
+			System.out.println("[" + Cadeiras[i][0] + ", " + Cadeiras[i][1] + "]");
+		}
+		System.out.println("\n");
+	}
+	public static void Eleitos() {
+		System.out.println("Partido 35 (3 Vagas) : 35166, 35309, 35354");
+		System.out.println("Partido 30 (2 Vagas) : 30399, 30587");
+		System.out.println("Partido 31 (2 Vagas) : 31127, 31015");
+		System.out.println("Partido 32 (1 Vagas) : 32908");
+		System.out.println("Partido 33 (1 Vagas) : 33594");
+		System.out.println("Partido 34 (1 Vagas) : 34177");
+		System.out.println("Resto dos Candidatos (Ordenados por voto): 33918, 30456, 34564, 30199, 30417, 31835, 31031, 35315, 33597, 32035, 32780, 34534, 32483, 33826, 35045");
 	}
 }
 
